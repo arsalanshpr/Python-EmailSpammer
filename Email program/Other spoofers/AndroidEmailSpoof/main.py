91f1dbb530c5b02f72c9fed05ca7267d3769db39
Kresten Laust Faaborg Sckerl <kressckerl@gmail.com>
2017-09-27 14:18:12 +0200

    More stuff

--- Email program/Other spoofers/AndroidEmailSpoof/main.py
+++ Email program/Other spoofers/AndroidEmailSpoof/main.py
@@ -2,16 +2,19 @@ import smtplib
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
-
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 class EmailSpoof(App):
 	def build(self):
-		return Button(text="Testing text")
+		return Button(text="hello world").bind(on_press=callback)
+	
 		
 def callback(instance):
 	print("Hello world" % instance.text)
 	
 b = Button(text="Test1")
 b.bind(on_press=callback)
+textinput = TextInput(text='Hello world')
 
 EmailSpoof().run()
