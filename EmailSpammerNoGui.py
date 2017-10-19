547cb4c4d67605784adde53edcb6107ba531e91e
Nett18 <kressckerl@gmail.com>
2017-10-19 14:17:52 +0200

    Created the No Gui version.

---
+++ EmailSpammerNoGui.py
@@ -0,0 +1,30 @@
+import smtplib, getpass, os, sys, subprocess
+amount = 1
+fromemail = "example@example.com"
+username = "example@example.com"
+password = "password"
+smtpserver = '"smtp.gmail.com:587"'
+fromemail = raw_input("Please enter your email: ")
+print ("Please enter your password(the text will be hidden): ")
+password = getpass.getpass()
+smtpserver = raw_input("Enter smtpserver, googles is: smtp.gmail.com:587: ")
+toemail = raw_input("Please enter the target email: ")
+message = raw_input("Please enter your nice message: ")
+amount = raw_input("Please enter amount, a nice start is 50: ")
+#insert more here
+
+#send function VVV
+server=smtplib.SMTP(smtpserver)
+server.starttls()
+server.login(fromemail, password)
+server.sendmail(fromemail, toemail, message)
+print "Sending "+amount+" emails"
+i = 1
+while i < int(amount):
+    print(i)
+    server.sendmail(fromemail, toemail, message)
+    i = i+1
+server.quit()
+pause = raw_input("Press ENTER to restart the program, or just exit")
+os.startfile("EmailSpammerNoGui.py")
+os.system('kill $PPID')
